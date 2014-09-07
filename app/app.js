2690729740c7b9a6664c1ad34db21bb0c9131866
Charles Julian Knight <charles@rabidaudio.com>
2014-09-07 12:56:34 -0400

    fixed app to use mongo

--- app/app.js
+++ app/app.js
@@ -5,24 +5,8 @@ var exec = require('child_process').exec;
 
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
-    if(err) throw err;
+var collection;
 
-    var collection = db.collection('test_insert');
-    collection.insert({a:2}, function(err, docs) {
-
-      collection.count(function(err, count) {
-        console.log(format("count = %s", count));
-      });
-
-      // Locate all the entries using find
-      collection.find().toArray(function(err, results) {
-        console.dir(results);
-        // Let's close the db
-        db.close();
-      });
-    });
-  })
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -33,24 +17,50 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/band_info', function(req, res){
     console.log(req.query);
-    var band = req.query.band;
-    var state = req.query.state;
+    // var band = req.query.band;
+    // var state = req.query.state;
 //    var result = sh.exec('ruby ../main.rb "'+artist+'" "'+state+'"');
-	res.send("hello world");
-return;
-child = exec('ruby ../main.rb "'+artist+'" "'+state+'"',
-  function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }else{
-	res.send(stdout);
-    }
-});
+
+  // child = exec('ruby ../main.rb "'+artist+'" "'+state+'"',
+  //   function (error, stdout, stderr) {
+  //     console.log('stdout: ' + stdout);
+  //     console.log('stderr: ' + stderr);
+  //     if (error !== null) {
+  //       console.log('exec error: ' + error);
+  //     }else{
+  // 	res.send(stdout);
+  //     }
+  // });
    // res.send(result.stdout);
+
+  //  collection.find_one().toArray(function(err, results) {
+  //   if(err) throw err;
+  //   // console.dir(results);
+  //   // Let's close the db
+
+  // });
+  collection.findOne(req.query, function(err, doc){
+    if(err) throw err;
+    res.send(doc);
+  });
 });
 
 
-app.listen(80);
-console.log("running");
+MongoClient.connect('mongodb://127.0.0.1:27017/rockscor', function(err, db) {
+    if(err) throw err;
+
+    collection = db.collection('artists');
+    // collection.insert({a:2}, function(err, docs) {
+
+      // collection.count(function(err, count) {
+      //   console.log(format("count = %s", count));
+      // });
+
+      // Locate all the entries using find
+      app.listen(80);
+      console.log("running");
+  });
+
+
+
+
--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,1 +1,40 @@
 
+
+var temlates = {
+    band_info: '
+    <h3>{{name}}</h3>
+    <p>{{location}}</p>
+    <p>
+    {{#list tags}}<a href="{{url}}">{{url}}</a>{{/list}}
+    </p>',
+
+
+
+}
+
+
+
+Handlebars.registerHelper('list', function(items, options) {
+  var out = "<ul>";
+
+  for(var i=0, l=items.length; i<l; i++) {
+    out = out + "<li>" + options.fn(items[i]) + "</li>";
+  }
+
+  return out + "</ul>";
+});
+
+
+
+
+$(document).ready(function(){
+    var band_info = Handlebars.compile(temlates.band_info);
+
+    $.ajax("/band_info"+window.location.search).done(function(data){
+        console.log(data);
+        //todo update ui wuth data
+    });
+
+});
+
+
