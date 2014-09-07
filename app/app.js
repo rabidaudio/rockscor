f67233042105e1d079f381389596fe2be2b6091e
Charles Julian Knight <charles@rabidaudio.com>
2014-09-07 12:02:00 -0400

    added frontend

---
+++ app/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var sh = require('execSync');
+var logger = require('morgan');
+
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(logger);
+// app.use(express.bodyParser());
+
+
+
+app.get('/band_info', function(req, res){
+    console.log(req.query);
+    var band = req.query.band;
+    var state = req.query.state;
+    var result = sh.exec('ruby ../main.rb "'+artist+'" "'+state+'"');
+    res.send(result.stdout);
+});
+
+
+app.listen(8080);
+console.log("running");
